7bd2493f5dd1fa78491b5dcd0e4ea6400ca1b13c
Hong SeongHak <tjdgkr0719wkr@gmail.com>
2023-01-18 16:49:31 +0900

    ex_data_modify

--- AWPS CRAWLLING/jumpit.py
+++ AWPS CRAWLLING/jumpit.py
@@ -1,5 +1,5 @@
 import requests  # 모듈들 불러오기
-from bs4 import BeautifulSoup
+import bs4
 from selenium import webdriver
 import time
 from selenium.common.exceptions import NoSuchElementException
@@ -49,28 +49,24 @@ while True:
     dic['회사제목'] = company_title_tag1 + company_title_cont + company_title_tag2
     
     # 정보 전체 다 긁어오기
-    for i in range(1,11) :
-        try:
-            info_title = driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[2]/dl[{i}]/dt').text
-            info_title_tag1 ='<' +  driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[2]/dl[{i}]/dt').tag_name+ '>'
-            info_title_tag2 ='</' +  driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[2]/dl[{i}]/dt').tag_name+ '>'
-            info_phrase = driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[2]/dl[{i}]/dd/pre').text
-            info_phrase_tag1 = '<' + driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[2]/dl[{i}]/dd/pre').tag_name+ '>'
-            info_phrase_tag2 = '</' + driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[2]/dl[{i}]/dd/pre').tag_name+ '>'
-            dic[info_title_tag1 + info_title + info_title_tag2] = info_phrase_tag1 + info_phrase + info_phrase_tag2
-        
-        except NoSuchElementException :
-            pass
+    
+
+    
+    source = driver.page_source
+    bs = bs4.BeautifulSoup(source,'lxml')
+    entire = bs.find('section', class_ = 'styles_position_info__96hrw styles_skeleton__hl_0i')
+    dic['본문'] = str(entire)
+    
 
     for i in range(1,11) :
         try :
             DetailInfo_title = driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dt').text
-            DetailInfo_title_tag1 = '<' +driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dt').tag_name+ '>'
-            DetailInfo_title_tag2 = '</' +driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dt').tag_name+ '>'
+            # DetailInfo_title_tag1 = '<' +driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dt').tag_name+ '>'
+            # DetailInfo_title_tag2 = '</' +driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dt').tag_name+ '>'
             DetailInfo_cont = driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dd').text
-            DetailInfo_cont_tag1 = '<' +driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dd').tag_name+ '>'
-            DetailInfo_cont_tag2 = '</' +driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dd').tag_name+ '>'
-            dic[DetailInfo_title_tag1 + DetailInfo_title + DetailInfo_title_tag2] = DetailInfo_cont_tag1 + DetailInfo_cont + DetailInfo_cont_tag2
+            # DetailInfo_cont_tag1 = '<' +driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dd').tag_name+ '>'
+            # DetailInfo_cont_tag2 = '</' +driver.find_element(By.XPATH, f'//*[@id="root"]/main/div/div[2]/div/section[3]/dl[{i}]/dd').tag_name+ '>'
+            dic[DetailInfo_title ] =DetailInfo_cont 
         except NoSuchElementException:
             pass
 
