dc1e4eb31988fc4e04f9492aeb4bbed7cfdd9043
Hong SeongHak <tjdgkr0719wkr@gmail.com>
2023-02-24 20:24:57 +0900

    programmers_processed_data_py_23/02/24

--- Processed_DAta/processed_data_programmers.py
+++ Processed_DAta/processed_data_programmers.py
@@ -10,33 +10,37 @@ dic = {}
 item_list = ['id','연봉','경력','회사이름']
 items = response['Items']
 for item in items:
-    for x in item_list:
+    salary = None
+    for i in item_list:
         try:
-            if x == '경력':
-                career = item[x]
-                #if career == "경력 무관":
-                    #dic["경력"] = "경력 무관"
-                #else:
-                career = career.replace("~", "").replace(" 년", "")
-                minimum, maximum = career.split()
-                dic["최소 경력"] = int(minimum)
-                dic["최대 경력"] = int(maximum)
-
-            elif x == '연봉':
-                salary = item[x]
+            if i == 'id' :
+                dic['id'] = int(item[i])
+            elif i == '연봉':
+                salary = item[i]
                 if salary:
                     salary = salary.replace("~", "").replace(" 만원", "")
                     minimum, maximum = salary.split()
-                    dic["최소"] = int(minimum)
-                    dic["최대"] = int(maximum)
-                else:
-                    dic["연봉"] = "no data"
+                    dic["최소 연봉"] = int(minimum)
+                    dic["최대 연봉"] = int(maximum)
+                    
+
+            elif i == '경력':
+                career = item[i]
+                #if career == "경력 무관":
+                    #dic["경력"] = "경력 무관"
+                #else:
+                career = career.replace("~", "").replace("년", "")
+                mininum , maxinum = career.split()
+                dic["최소 경력"] = int(mininum)
+                dic["최대 경력"] = int(maxinum)
+            
             else:
                 pass
-            print(dic)
+
         except:
             pass
-
+    print(dic)
+    dic = {}
 #for item in items:
     #print(f"id: {item['id']}, career: {item['경력']}, employment type: {item['고용 형태']}, technical stack: {item['기술스택']}, body: {item['본문']}, salary: {item['연봉']}, application deadline: {item['지원 마감']}, job: {item['직무']}, company name: {item['회사이름']}, company title: {item['회사타이틀']}")
     #x+=1
