417f23932755bcf9cdddd05cb806b09528e83b3d
Hong SeongHak <tjdgkr0719wkr@gmail.com>
2023-02-26 00:55:39 +0900

    ProcessedData_programmers.V2

--- Processed_DAta/processed_data_programmers.py
+++ Processed_DAta/processed_data_programmers.py
@@ -7,7 +7,7 @@ table = dynamodb.Table('programmers')
 response = table.scan()
 x=0
 dic = {}
-item_list = ['id','연봉','경력','회사이름']
+item_list = ['id','연봉','경력','기술스택','직무']
 items = response['Items']
 for item in items:
     salary = None
@@ -15,32 +15,42 @@ for item in items:
         try:
             if i == 'id' :
                 dic['id'] = int(item[i])
+
             elif i == '연봉':
                 salary = item[i]
                 if salary:
                     salary = salary.replace("~", "").replace(" 만원", "")
-                    minimum, maximum = salary.split()
-                    dic["최소 연봉"] = int(minimum)
-                    dic["최대 연봉"] = int(maximum)
-                    
-
+                    payList = salary.split()
+                    dic["연봉"] = list(map(int, payList))
+                   
             elif i == '경력':
                 career = item[i]
                 if career == "경력 무관":
                     dic["경력"] = "경력 무관"
                 else:
                     career = career.replace("~", "").replace("년", "")
-                    mininum , maxinum = career.split()
-                    dic["최소 경력"] = int(mininum)
-                    dic["최대 경력"] = int(maxinum)
-            
+                    careerLsit = career.split()
+                    dic["경력"] = list(map(int, careerLsit))
+
+            elif i == '기술스택' :
+                stacks_list = item[i].split(',').pop()
+                dic['기술스택'] = stacks_list
+                
+            elif i == '직무' :
+                JobDuty = item[i].split(',')
+                dic['직무'] = JobDuty
+
             else:
                 pass
 
-        except:
+        except :
             pass
+
+        
     print(dic)
-    table = dynamodb.Table("Processed_programmers")
-    table.put_item(Item=dic)
+    # table = dynamodb.Table("Processed_programmers")
+    # table.put_item(Item=dic)
     dic = {}
+    print(x)
+    x+=1
 
