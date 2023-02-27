4dd9a18eda8dbd4d7ba8cdd1657c1a4bec3d9c2f
Sabana6Mana <109522731+Sabana6Mana@users.noreply.github.com>
2023-02-27 19:52:58 +0900

    processed_jumpit_to_s3

--- Processed_DAta/processed_data_jumpit.py
+++ Processed_DAta/processed_data_jumpit.py
@@ -1,15 +1,19 @@
 import boto3
+import json
+import datetime
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2',aws_access_key_id='AKIATMPH7BYJ7OEGGY5J',aws_secret_access_key = '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')
 
 table = dynamodb.Table('jumpit')
-
+access_key = 'AKIATMPH7BYJ7OEGGY5J'
+secret_key = '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'
 response = table.scan()
 
 x=0
 dic = {}
 item_list = ['id','회사이름', '연봉','경력','기술스택','회사제목','']
 items = response['Items']
+s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
 
 #scan()이 한번에 가져올수 있는 용량 제한이 있어 용량 걸렸을때 리스트 확장하는 코드
 while 'LastEvaluatedKey' in response:
@@ -17,6 +21,8 @@ while 'LastEvaluatedKey' in response:
     items.extend(response['Items'])
 
 for item in items:
+    now = datetime.datetime.now()
+    ttime = now.strftime("%y-%m-%d %H:%M:%S")
     salary = None
     for i in item_list:
         try:
@@ -69,10 +75,13 @@ for item in items:
         except :
             pass
 
-        
+    dic['시간'] = ttime
     print(dic)
+    json_data = json.dumps(dic, indent=2, ensure_ascii=False)
+    bucket_name = 'processedjumpit'
+    file_key = f"{dic['id']}.json"
     table = dynamodb.Table("Processed_jumpit")
-    table.put_item(Item=dic)
+    s3.put_object(Bucket=bucket_name, Key=file_key, Body=json_data)
     dic = {}
     print(x)
     x+=1
