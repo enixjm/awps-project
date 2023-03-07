d508afdd746aa5ea9896a37eb8a43ab4a2855382
enixjm <cys08190819@gmail.com>
2023-03-07 17:15:25 +0900

    feature add load csv

--- DataAnalysis/main.py
+++ DataAnalysis/main.py
@@ -1,38 +1,7 @@
-import os
-
-import boto3
 import pandas as pd
-import json
 
-s3 = boto3.resource('s3')
-bucket_name = 'processedjumpit'
+df = pd.read_csv('all_data.csv', index_col=0)
+df['id'] = df['id'].astype(int)
+df = df.set_index('id')
 
-bucket = s3.Bucket(bucket_name)
-
-df = pd.DataFrame()
-
-for obj in bucket.objects.all():
-    json_string = obj.get()['Body'].read().decode('utf-8')
-    json_dict = json.loads(json_string)
-    df = df.append(json_dict, ignore_index=True)
-    print(df)
-
-# obj = s3.Object(bucket_name, '9991.json')
-# response = obj.get()
-
-# print(response['Body'].read().decode('utf-8'))
-
-
-
-# s3_client = boto3.client("s3")
-
-# response = s3_client.get_object(Bucket='processedjumpit', Key="10026.json")
-
-# status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
-
-# if status == 200:
-#     print(f"Successful S3 get_object response. Status - {status}")
-#     books_df = pd.read_csv(response.get("Body"))
-#     print(books_df)
-# else:
-#     print(f"Unsuccessful S3 get_object response. Status - {status}")
+print(df)
