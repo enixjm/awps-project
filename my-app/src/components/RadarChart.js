0ae6d2bd03bcb87b0005c0d1225ae49ee4dc6006
Sabana6Mana <109522731+Sabana6Mana@users.noreply.github.com>
2023-03-11 17:57:29 +0900

    Create RadarChart.js

---
+++ my-app/src/components/RadarChart.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import {Pie, Radar} from 'react-chartjs-2';
+
+function RadarChart({ chartData }) {
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+  return <Radar data={chartData} options={options} />;
+}
+
+export default RadarChart;
